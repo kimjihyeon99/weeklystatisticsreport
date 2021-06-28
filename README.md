202505f4ef19cb54d43dcfc969d1af15744a3a14
kimjihyeon99 <44187194+kimjihyeon99@users.noreply.github.com>
2021-06-28 10:29:09 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# weeklystatisticsreport
+ 백마인턴십 프로젝트
---
+++ android/app/src/main/kotlin/com/example/weeklystatisticsreport/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.weeklystatisticsreport
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
