934a1e3cf0119b757460ae1d68a3f450b6cb038a
kimjihyeon99 <44187194+kimjihyeon99@users.noreply.github.com>
2021-08-06 17:45:11 +0900

    make zoomdrawer

---
+++ weeklystatisticsreport/lib/main_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
+import 'menu_screen.dart';
+import 'mainmenu.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+          fontFamily: 'bitro',
+          textTheme: TextTheme(
+              bodyText1: TextStyle(
+                  fontSize: 15.0,
+                  color: Color(0xff3C5186),
+                  fontWeight: FontWeight.bold)),
+          primaryColor: PrimaryColor,
+          canvasColor: SecondColor),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final drawerController = ZoomDrawerController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ZoomDrawer(
+        controller: drawerController,
+        style: DrawerStyle.Style1,
+        menuScreen: MenuScreen(),
+        mainScreen: mainmenuPage(drawerController),
+        borderRadius: 24.0,
+        showShadow: true,
+        angle: 0.0,
+        backgroundColor: Colors.grey[300],
+        slideWidth: MediaQuery.of(context).size.width*.5,
+        openCurve: Curves.fastOutSlowIn,
+        closeCurve: Curves.easeInBack,
+      ),
+    );
+  }
+}
